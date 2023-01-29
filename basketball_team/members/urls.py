e55e1b9bb54b1064c8c6da39a9fd144100932199
NABIN BHATTARAI <95476351+NABIN720@users.noreply.github.com>
2023-01-29 21:37:11 +0545

    Delete basketball_team directory

--- basketball_team/members/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('membeers/', views.membeers,name='membeers'),
-    path('membeers/details/<int:id>', views.details,name='details'),
-]
